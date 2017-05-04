dc68df756a2b9c9d3aaa9b2ef88f5f7d5c25d89e
Xianhua Lei <xianhua.lei@istuary.com>
2017-05-04 17:57:56 +0800

    add automatically initiate database and fixed bug

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -1,22 +1,20 @@
- 'use strict';                                    
- /**                                              
- * db config                                     
-  * @type {Object}                                 
- */                                               
- export default {                                
-      type: 'mysql',                              
-      adapter: {                                  
-           mysql: {                             
-               host: '127.0.0.1',                
-              port: '3306',                     
-              database: 'mockserver2ww',           
-              user: 'root',                     
-              password: '',                     
-              prefix: 'mock_',                  
-             encoding: 'UTF8MB4_GENERAL_CI'     
-          },                                    
-         mongo: {                                 
-                                                  
-         }                                        
-     }                                            
- };                                               
+'use strict';
+/**
+ * db config
+ * @type {Object}
+ */
+export default {
+    type: 'mysql',
+    adapter: {
+        // mysql: {
+        //     host: '127.0.0.1',
+        //     port: '3306',
+        //     database: 'mockserver',
+        //     user: 'root',
+        //     password: '',
+        //     prefix: 'mock_',
+        //     encoding: 'UTF8MB4_GENERAL_CI'
+        // },
+        mongo: {}
+    }
+};
