2928ef8791a57f37c2bf2fe891d58fb92c1765c8
Xianhua Lei <xianhua.lei@istuary.com>
2017-05-15 10:07:26 +0800

    add express route style

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -6,15 +6,15 @@
 export default {
     type: 'mysql',
     adapter: {
-        mysql: {
-            // host: '127.0.0.1',
-            // port: '3306',
-            // database: 'mockserver',
-            // user: 'root',
-            // password: '',
-            // prefix: 'mock_',
-            // encoding: 'UTF8MB4_GENERAL_CI'
-        },
+        // mysql: {
+        //     // host: '127.0.0.1',
+        //     // port: '3306',
+        //     // database: 'mockserver',
+        //     // user: 'root',
+        //     // password: '',
+        //     // prefix: 'mock_',
+        //     // encoding: 'UTF8MB4_GENERAL_CI'
+        // },
         mongo: {}
     }
 };
