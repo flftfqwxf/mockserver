d91025ddfb55c5fca73750eaa80928688d649192
Xianhua Lei <xianhua.lei@istuary.com>
2017-05-18 15:52:24 +0800

    fixed bug

---
+++ src/common/config/db.js
@@ -0,0 +1,19 @@
+'use strict';
+/**
+ * db config
+ * @type {Object}
+ */
+export default {
+    type: 'mysql',
+    adapter: {
+        // mysql: {
+        //     host: '127.0.0.1',
+        //     port: '3306',
+        //     database: 'mockserver',
+        //     user: 'root',
+        //     password: '',
+        //     prefix: 'mock_',
+        //     encoding: 'UTF8MB4_GENERAL_CI'
+        // },
+    }
+};
