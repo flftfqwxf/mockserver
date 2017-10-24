48ed4185920700543b48f191bbc7ac0cdc8cf21c
Xianhua Lei <xianhua.lei@istuary.com>
2017-10-24 17:35:55 +0800

    swagger

--- mockserver.sql
+++ mockserver.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.14)
 # Database: mockserver
-# Generation Time: 2017-07-17 09:25:25 +0000
+# Generation Time: 2017-10-24 09:32:35 +0000
 # ************************************************************
 
 
@@ -47,22 +47,23 @@ CREATE TABLE `mock_mockserver` (
   `api_querys_desc` longtext COMMENT '接口请求url中？后参数说明',
   `api_req_header` longtext COMMENT '接口响应HEADER参数',
   `api_req_header_desc` longtext COMMENT '接口响应HEADER参数说明',
+  `swagger_url` varchar(2000) DEFAULT NULL COMMENT '接口对应的swagger 接口路径，用于在swagger tab中，显示指定接口',
   PRIMARY KEY (`mockid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `mock_mockserver` WRITE;
 /*!40000 ALTER TABLE `mock_mockserver` DISABLE KEYS */;
 
-INSERT INTO `mock_mockserver` (`mockid`, `project_id`, `api_name`, `api_url`, `api_url_regexp`, `api_content`, `api_content_desc`, `is_proxy`, `proxy_prefix`, `api_header`, `api_header_desc`, `api_parms`, `api_parms_desc`, `api_type`, `api_state_code`, `exact_match`, `is_mockjs`, `api_lazy_time`, `api_querys_desc`, `api_req_header`, `api_req_header_desc`)
+INSERT INTO `mock_mockserver` (`mockid`, `project_id`, `api_name`, `api_url`, `api_url_regexp`, `api_content`, `api_content_desc`, `is_proxy`, `proxy_prefix`, `api_header`, `api_header_desc`, `api_parms`, `api_parms_desc`, `api_type`, `api_state_code`, `exact_match`, `is_mockjs`, `api_lazy_time`, `api_querys_desc`, `api_req_header`, `api_req_header_desc`, `swagger_url`)
 VALUES
-	(85,'8a15fbb94471050bb46f','mockjs-demo-打开接口-刷新页面数据会随机变化','demo1',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',200,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}'),
-	(86,'8a15fbb94471050bb46f','404-code-status-demo-请求返回状态为：404','demo2',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',404,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}'),
-	(88,'8a15fbb94471050bb46f','dealy-3-second-demo-延迟3秒返回数据','dealy-3-second-demo',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',404,0,1,3000,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}'),
-	(89,'8a15fbb94471050bb46f','switch-to-real-data-点击关闭二次代理，将访问模拟数据，开启二次代理-访问真实服务的数据','demo/switch-to-real-data',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',1,'http://47.93.62.181:8034','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',404,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}'),
-	(98,'8a15fbb94471050bb46f','RESTful格式示例-修改 :id,:user试试，如 search/4/tom','search/:id/:name','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',200,0,0,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}'),
-	(104,'8a15fbb94471050bb46f','method为post RESTful格式示例-修改 :id,:user试试，如 search/4/tom','search/:id/:name','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"POST\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','post',200,0,0,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}'),
-	(105,'8a15fbb94471050bb46f','method为 put :RESTful格式示例-修改 :id,:user试试，如 search/4/tom','search/:id/:name','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"PUT\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','put',200,0,0,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}'),
-	(106,'8a15fbb94471050bb46f','method为 delete','search/:id/:name/ss','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))%5C/ss(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"PUT\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','delete',200,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}');
+	(85,'8a15fbb94471050bb46f','mockjs-demo-打开接口-刷新页面数据会随机变化','demo1',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',200,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL),
+	(86,'8a15fbb94471050bb46f','404-code-status-demo-请求返回状态为：404','demo2',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',404,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL),
+	(88,'8a15fbb94471050bb46f','dealy-3-second-demo-延迟3秒返回数据','dealy-3-second-demo',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',404,0,1,3000,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL),
+	(89,'8a15fbb94471050bb46f','switch-to-real-data-点击关闭二次代理，将访问模拟数据，开启二次代理-访问真实服务的数据','demo/switch-to-real-data',NULL,'{\r\n  \"array|1-10\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',1,'http://47.93.62.181:8034','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',404,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL),
+	(98,'8a15fbb94471050bb46f','RESTful格式示例-修改 :id,:user试试，如 search/4/tom','search/:id/:name','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"Hello World\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','get',200,0,0,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL),
+	(104,'8a15fbb94471050bb46f','method为post RESTful格式示例-修改 :id,:user试试，如 search/4/tom','search/:id/:name','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"POST\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','post',200,0,0,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL),
+	(105,'8a15fbb94471050bb46f','method为 put :RESTful格式示例-修改 :id,:user试试，如 search/4/tom','search/:id/:name','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"PUT\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','put',200,0,0,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL),
+	(106,'8a15fbb94471050bb46f','method为 delete','search/:id/:name/ss','%5Esearch%5C/((?:%5B%5E%5C/%5D+?))%5C/((?:%5B%5E%5C/%5D+?))%5C/ss(?:%5C/(?=$))?$','{\r\n  \"array\": [\r\n    1,\r\n    2,\r\n    3\r\n  ],\r\n  \"boolean\": true,\r\n  \"null\": null,\r\n  \"number\": 123,\r\n  \"object\": {\r\n    \"a\": \"b\",\r\n    \"c\": \"d\",\r\n    \"e\": \"f\"\r\n  },\r\n  \"string\": \"PUT\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}',0,'','{\r\n  \"headere1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}','{\r\n  \"username\": \"参数值\",\r\n  \"password\": \"参数值\"\r\n}','{\r\n  \"username\": \"参数说明\",\r\n  \"password\": \"参数说明\"\r\n}','delete',200,0,1,0,'{\r\n  \"query1\": \"参数说明\",\r\n  \"query2\": \"参数说明\"\r\n}','{\r\n  \"header1\": \"参数值\",\r\n  \"header2\": \"参数值\"\r\n}','{\r\n  \"headere1\": \"参数说明\",\r\n  \"header2\": \"参数说明\"\r\n}',NULL);
 
 /*!40000 ALTER TABLE `mock_mockserver` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -76,21 +77,48 @@ DROP TABLE IF EXISTS `mock_project`;
 CREATE TABLE `mock_project` (
   `project_id` varchar(20) NOT NULL DEFAULT '',
   `project_name` varchar(2000) NOT NULL DEFAULT '',
-  `project_prefix` varchar(200) DEFAULT '/api/',
+  `project_prefix` varchar(200) DEFAULT '/',
   `proxy_url` varchar(2000) DEFAULT '',
   `open_proxy` int(1) NOT NULL DEFAULT '1',
+  `swagger_url` varchar(2000) DEFAULT NULL COMMENT 'swagger获取接口信息路径',
   PRIMARY KEY (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `mock_project` WRITE;
 /*!40000 ALTER TABLE `mock_project` DISABLE KEYS */;
 
-INSERT INTO `mock_project` (`project_id`, `project_name`, `project_prefix`, `proxy_url`, `open_proxy`)
+INSERT INTO `mock_project` (`project_id`, `project_name`, `project_prefix`, `proxy_url`, `open_proxy`, `swagger_url`)
 VALUES
-	('8a15fbb94471050bb46f','demo','/','http://192.168.1.2',1),
-	('bb1ddc9459253909d379','swift-demo','/api/','',1);
+	('8a15fbb94471050bb46f','demo','/','http://192.168.1.2',1,''),
+	('bb1ddc9459253909d379','sdfsdfsdf','/','',1,'http://127.0.0.1:3000/api-docs.json'),
+	('f5e8f1d510cc6e0be603','笔记','/','http://192.168.28.218:8088',1,NULL);
 
 /*!40000 ALTER TABLE `mock_project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table mock_swagger
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mock_swagger`;
+
+CREATE TABLE `mock_swagger` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `header_name` varchar(2000) DEFAULT NULL COMMENT '需要在请求中添中的header字段名,如cookie',
+  `header_val` varchar(2000) DEFAULT NULL COMMENT '需要在请求中添中的header字段值',
+  `type` int(2) DEFAULT NULL COMMENT 'swagger类型,1为全局，2为单个',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `mock_swagger` WRITE;
+/*!40000 ALTER TABLE `mock_swagger` DISABLE KEYS */;
+
+INSERT INTO `mock_swagger` (`id`, `header_name`, `header_val`, `type`)
+VALUES
+	(1,'cookie','ggg',1),
+	(2,'cookie','ggg',1);
+
+/*!40000 ALTER TABLE `mock_swagger` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
